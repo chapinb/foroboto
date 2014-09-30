a95645d0819bf664e8f8ef6ccc683550a4634112
Chapin Bryce <4n6kid@users.noreply.github.com>
2014-09-30 01:09:46 -0400

    Added code and updates to README

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 Foroboto
 ======
 
-ADB Acquisition Tool for Android Forensics
+**ADB Acquisition Tool for Android Forensics**
+
+*Copyright (C) 2014, Chapin Bryce*
+
+# Installation & Dependencies
+
+Place the `adb.exe`, `AdbWinApi.dll`, and `AdbWinUsbApi.dll` in the same directory of the `foroboto.bat` script. These dependencies can be downloaded from [Google's SDK Site](http://developer.android.com/sdk/index.html). Be sure to locate the `adb.exe` and either copy the noted dependencies into the same directory as `foroboto.bat` or copy `foroboto.bat` into the directory containing `adb.exe` 
 
 # Usage
 
@@ -10,3 +16,9 @@ Run `foroboto.bat` on a Windows based system
 1. Android must be plugged in and have ADB USB Debugging Enabled
 2. For Level 4 and 5 Acquisition, root access is required
 3. Device must be authenticated by device for connection
+
+# More Information
+
+Visit the project's development page on GitHub: https://github.com/chapinb/foroboto
+Leave any feature requests or bugs found in the issues tab.
+Wiki page coming soon!
---
+++ foroboto.bat
@@ -0,0 +1,198 @@
+rem    ADB Acquisition and processing script
+rem    Copyright (C) 2014 Chapin Bryce
+rem	   You can find more code at github.com/chapinb
+rem
+rem    This program is free software: you can redistribute it and/or modify
+rem    it under the terms of the GNU General Public License as published by
+rem    the Free Software Foundation, either version 3 of the License, or
+rem    (at your option) any later version.
+rem
+rem    This program is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem    GNU General Public License for more details.
+rem
+rem    You should have received a copy of the GNU General Public License
+rem    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+rem Prompt user for Report Information
+@ECHO off
+ECHO [INFO] Starting Roboto...
+:err1
+cls
+set /p RPTNUM=Type in report # (i.e. 9999):
+ECHO You typed: "%RPTNUM%"
+set/p _RNV=Is this correct? (y/n)
+If /i "%_RNV%"=="n" goto :err1
+cls
+
+rem Prompt user for ADB Location [Depreciated]
+rem :err2
+rem ECHO [USER] Select adb.exe location
+rem vset /p ADBPATH=Type in the full path to adb.exe ("C:\adt\adb.exe"):
+rem echo You specified "%ADBPATH%"
+rem set /p _CLV=Is this correct? (y/n)
+rem If /i "%_CLV%"=="n" goto :err2
+rem if exist {%ADBPATH%} (
+	rem file exists
+rem ) else ( 
+rem	echo Invalid path, try again
+rem	goto :err2
+
+rem cls
+
+SET _ADBPATH=adb.exe
+
+
+rem Prompt user for run level
+:err3
+cls
+ECHO [USER] Select the run level you wish to execute:
+ECHO 	1. Collect live information (Dumpstate + Logcat)
+ECHO 	2. Level 1 + System information 
+ECHO 	3. Level 2 + Logical acquisition of the SD Card
+ECHO 	4. Level 3 + Logical acquisition of the Data directory
+ECHO 	5. Level 4 + Full logical acuisition (All local directories)
+set /p COLLVL=Type in the collection level (1-5):
+echo You selected level "%COLLVL%"
+set /p _CLV=Is this correct? (y/n)
+If /i "%_CLV%"=="n" goto :err3
+cls
+
+rem Make directory to output to
+mkdir %RPTNUM%
+
+rem Check ADB devices
+:err4
+echo [USER] Ensure the android device is connected and ADB Debugging is enabled on the device.
+set Press enter to check for connected ADB Devices
+%_ADBPATH% devices
+%_ADBPATH% root
+set /p _CHK=Is the correct device connected? (y/n)
+If /i "%_CHK%"=="n" goto :err4
+cls
+
+ECHO **************************************************
+ECHO **************************************************
+echo This collection is starting
+echo This may take a few minutes
+ECHO **************************************************
+ECHO **************************************************
+
+rem Start processing Level 1
+
+echo [INFO] Running dumpstate
+%_ADBPATH% shell dumpstate > %RPTNUM%/dumpstate.txt
+
+echo [INFO] Running logcat
+%_ADBPATH% shell logcat -d -v long V:* > %RPTNUM%/logcat.txt
+
+
+rem Go to run level
+If /i "%COLLVL%"=="1" goto :lvl1 
+If /i "%COLLVL%"=="2" goto :lvl2
+mkdir %RPTNUM%\fs-pull
+If /i "%COLLVL%"=="3" goto :lvl3
+If /i "%COLLVL%"=="4" goto :lvl4
+If /i "%COLLVL%"=="5" goto :lvl5
+
+
+rem Start processing Level 5
+:lvl5
+
+echo [INFO] Acquiring Acct Partition
+mkdir %RPTNUM%\fs-pull\acct
+%_ADBPATH% pull /acct/ %RPTNUM%/fs-pull/acct/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Cache Partition
+mkdir %RPTNUM%\fs-pull\cache
+%_ADBPATH% pull /cache/ %RPTNUM%/fs-pull/cache/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Charger Partition
+mkdir %RPTNUM%\fs-pull\charger
+%_ADBPATH% pull /charger/ %RPTNUM%/fs-pull/charger/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Config Partition
+mkdir %RPTNUM%\fs-pull\config
+%_ADBPATH% pull /config/ %RPTNUM%/fs-pull/config/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring d Partition
+mkdir %RPTNUM%\fs-pull\d
+%_ADBPATH% pull /d/ %RPTNUM%/fs-pull/d/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Etc Partition
+mkdir %RPTNUM%\fs-pull\etc
+%_ADBPATH% pull /etc/ %RPTNUM%/fs-pull/etc/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Mnt Partition
+mkdir %RPTNUM%\fs-pull\mnt
+%_ADBPATH% pull /mnt/ %RPTNUM%/fs-pull/mnt/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Res Partition
+mkdir %RPTNUM%\fs-pull\res
+%_ADBPATH% pull /res/ %RPTNUM%/fs-pull/res/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Root Partition
+mkdir %RPTNUM%\fs-pull\root
+%_ADBPATH% pull /root/ %RPTNUM%/fs-pull/root/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring SBin Partition
+mkdir %RPTNUM%\fs-pull\sbin
+%_ADBPATH% pull /sbin/ %RPTNUM%/fs-pull/sbin/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Sys Partition
+mkdir %RPTNUM%\fs-pull\sys
+%_ADBPATH% pull /sys/ %RPTNUM%/fs-pull/sys/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring System Partition
+mkdir %RPTNUM%\fs-pull\system
+%_ADBPATH% pull /system/ %RPTNUM%/fs-pull/system/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+echo [INFO] Acquiring Tombstones Partition
+mkdir %RPTNUM%\fs-pull\tombstones
+%_ADBPATH% pull /tombstones/ %RPTNUM%/fs-pull/tombstones/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+rem Start processing Level 4
+:lvl4
+
+echo [INFO] Acquiring Data Partition
+mkdir %RPTNUM%\fs-pull\d
+%_ADBPATH% pull /data/ %RPTNUM%/fs-pull/data/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+rem Start processing Level 3
+:lvl3
+
+echo [INFO] Acquiring SD Card
+mkdir %RPTNUM%\fs-pull\sdcard
+%_ADBPATH% pull /sdcard/ %RPTNUM%/fs-pull/sdcard/ >> %RPTNUM%/fs-pull.log.txt  2>&1
+
+rem Start processing Level 2
+:lvl2
+
+echo [INFO] Running mount
+%_ADBPATH% shell mount > %RPTNUM%/mount.txt
+
+echo [INFO] Running netstat
+%_ADBPATH% shell netstat > %RPTNUM%/netstat.txt
+
+echo [INFO] Running netcfg
+%_ADBPATH% shell netcfg > %RPTNUM%/netcfg.txt
+
+echo [INFO] Running ifconfig
+%_ADBPATH% shell ifconfig > %RPTNUM%/ifconfig.txt
+
+echo [INFO] Running date
+%_ADBPATH% shell date > %RPTNUM%/date.txt
+
+echo [INFO] Running df
+%_ADBPATH% shell df > %RPTNUM%/df.txt
+
+echo [INFO] Running lsof
+%_ADBPATH% shell lsof > %RPTNUM%/lsof.txt
+
+echo [INFO] Running uptime
+%_ADBPATH% shell uptime > %RPTNUM%/uptime.txt
+
+:lvl1
+echo [INFO] Completed %RPTNUM%
